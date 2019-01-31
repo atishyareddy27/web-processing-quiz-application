5b03292905dc97fb631ef66a6e55c69710e0eb65
ATISHYA REDDY S <43718457+atishyareddy27@users.noreply.github.com>
2019-01-31 22:09:19 +0530

    Add files via upload

---
+++ quest.js
@@ -0,0 +1,33 @@
+var questions=[
+{
+"question":"Which of the following special symbol allowed in a variable name?",
+"option1":"",
+"option2":"",
+"option3":"",
+"option4":"",
+"answer":""
+},
+{
+"question":"What is (void*)0?",
+"option1":"Representation of NULL pointer",
+"option2":"Representation of void pointer",
+"option3":"Error",
+"option4":"None of above",
+"answer":'a'
+},
+{
+"question":"In C, if you pass an array as an argument to a function, what actually gets passed?",
+"option1":"Value of elements in array",
+"option2":"First element of the array",
+"option3":"Base address of the array",
+"option4":"Address of the last element of array",
+"answer":'c'
+},
+{
+"question":"How will you print \n on the screen??",
+"option1":"printf("\n");",
+"option2":"echo "\\n";",
+"option3":"printf('\n');",
+"option4":"printf("\\n")",
+"answer":'d'
+},
